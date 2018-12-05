231036750ed42618f2fcc20106bb3d18600adff3
Yifan Xu <xuy7@tcd.ie>
2018-12-05 16:31:06 +0000

    add luakit browser init config files

---
+++ luakit/userconf.lua
@@ -0,0 +1,9 @@
+local settings = require "settings"
+
+settings.window.home_page = "https://duckduckgo.com"
+
+settings.webview.enable_webgl = true
+settings.webview.enable_mediasource = true
+settings.webview.enable_media_stream = true
+settings.webview.enable_webaudio = true
+settings.webview.enable_accelerated_2d_canvas = true
